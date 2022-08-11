4fac45b06b17e5a00dfbacf37ac2086d1c3b71f0
Diyaeddine LAOUID <dilaouid@student.42.fr>
2022-08-11 21:01:40 +0200

    (+) Add a ping method to Database and header inside exit

--- Class/Database.php
+++ Class/Database.php
@@ -17,6 +17,17 @@
             $this->port = $port;
         }
 
+        // Try a connection to the database and returns true if the connection was established
+        public function tryConnection() {
+            try {
+                $this->mysql = new PDO('mysql:host='. $this->host .';port=' . $this->port . ';dbname=' . $this->dbname  . ';charset=utf8', $this->username, $this->username);
+                return (true);
+            } catch(Exception $e) {
+                return (false);
+            }
+        }
+
+        // Check if the database configuration is OK : good length for host, username and dbname
         private function checkDatabaseConfiguration() {
             $configDatas = [$this->host, $this->username, $this->dbname];
             array_walk($configDatas, function($el, $i) {
@@ -25,6 +36,7 @@
             });
         }
 
+        // Init a connection to the database according to the found ids
         public function init() {
             try {
                 $this->checkDatabaseConfiguration();
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-    if (is_dir('install')) {
-        header('Location: install/index.php');
-        exit();
-    }
+    // If the "install" directory exists, it means that the software
+    // is not installed, so let's redirect the user to the installation
+    if (is_dir('install'))
+        exit(header('Location: install/index.php'));
+    
     include("config/header_pdo.php");
 ?>
 
