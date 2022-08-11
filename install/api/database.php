14e7a415defe45d774479da0c990bfbf6e54af2a
Diyaeddine LAOUID <dilaouid@student.42.fr>
2022-08-11 21:02:46 +0200

    (+) init a db check route (first step of installation)

---
+++ install/api/database.php
@@ -0,0 +1,22 @@
+<?php
+    include ('header.php');
+    $post = protectAndTrim($_POST);
+    $emptyKeys = checkForKeys($post, ["host", "username"]);
+
+    if (count($emptyKeys) > 0) {
+        setResponse(
+            400,
+            "Tout les champs obligatoires doivent être remplis",
+            $emptyKeys,
+            false
+        );
+    }
+
+    $db = new Database($post['host'], $post['password'], $post['username'], '', is_numeric($post['port']) ?: 3306);
+    $success = $db->tryConnection();
+    setResponse(
+        $success ? 200 : 401,
+        $success ? 'OK' : "Les données de connexion à la base de données sont invalides",
+        $diff,
+        $success
+    );
