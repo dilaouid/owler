6b85efcee5080c89ad8062ff55bb360d63aa2569
Diyaeddine LAOUID <dilaouid@student.42.fr>
2022-08-11 21:03:12 +0200

    (+) creating an header for api routes in install directory

---
+++ install/api/header.php
@@ -0,0 +1,7 @@
+<?php
+    include ('../../api/utils.php');
+    include('../../Class/Database.php');
+    
+    // The API routes are all sending a json
+    header('Content-Type: application/json; charset=utf-8');
+?>
