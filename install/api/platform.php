b1a896fdaff2a8a2554bc4c4f346910949f00c98
Diyaeddine LAOUID <dilaouid@student.42.fr>
2022-08-12 09:09:18 +0200

    (+) check second step of the installation form

---
+++ install/api/platform.php
@@ -0,0 +1,11 @@
+<?php
+    include ('../../api/header.php');
+
+    checkForEmptyKeysForm($post, ["platform_name"]);
+    $success = strlen($post["platform_name"]) >= 3 && strlen($post["platform_name"]) <= 20;
+    setResponse(
+        $success ? 200 : 401,
+        $success ? 'OK' : "Le nom de votre plateforme de connexion doit faire entre 3 et 20 caractères.",
+        ["platform_name"],
+        $success
+    );
