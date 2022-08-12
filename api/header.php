7bcc3a4c2f2b10c5734f2d16d49739002df6b8d2
Diyaeddine LAOUID <dilaouid@student.42.fr>
2022-08-12 09:59:10 +0200

    (+) include pdo header if the config db file exists on api header

--- api/header.php
+++ api/header.php
@@ -1,7 +1,8 @@
 <?php
     include ('utils.php');
     include('../Class/Database.php');
-
+    if (file_exists('../config/db.php'))
+        include ('../config/header_pdo.php');
     // The API routes are all sending a json
     header('Content-Type: application/json; charset=utf-8');
 
