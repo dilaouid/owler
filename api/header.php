f23f3edd59ecd770f3258a72f60ef80450847e62
dilaouid <dilaouid@student.42.fr>
2022-08-14 18:44:42 +0200

    (~) setting dirname to locate files header.php

--- api/header.php
+++ api/header.php
@@ -1,8 +1,10 @@
 <?php
+    $dirname = dirname(__FILE__, 2);
     include ('utils.php');
-    include('../Class/Database.php');
-    if (file_exists('../config/db.php'))
-        include ('../config/header_pdo.php');
+    include($dirname . './Class/Database.php');
+    if (file_exists($dirname . '/config/db.php'))
+        include ($dirname . '/config/header_pdo.php');
+    
     // The API routes are all sending a json
     header('Content-Type: application/json; charset=utf-8');
 
